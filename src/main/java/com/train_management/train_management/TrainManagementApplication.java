2af6ad228ff871435d5109029c31e1da2d81adf3
takkamdarios <takkamdarios@gmail.com>
2024-02-28 17:38:56 +0100

    first commit

---
+++ src/main/java/com/train_management/train_management/TrainManagementApplication.java
@@ -0,0 +1,13 @@
+package com.train_management.train_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainManagementApplication.class, args);
+    }
+
+}
